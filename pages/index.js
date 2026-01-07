30b4c02cae6909b75a6f6823e59efda963a3faf2
paulosplima <155822549+paulosplima@users.noreply.github.com>
2026-01-07 09:46:46 -0300

    Update homepage message to 'EM CONSTRUÇÃO'

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 function HomePage() {
   return (
     <h3>
-      NOVA  !!! Em breve teremos muitas novidades para
+      EM CONSTRUÇÃO !!! Em breve teremos muitas novidades para
       você!Em breve teremos muitas novidades para você!
+        APENAS TREINANDO, POR ENQUANTO
     </h3>
   );
   <h3>Em breve teremos muitas novidades para você!</h3>;
