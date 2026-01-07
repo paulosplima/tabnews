74b2ba705502eb9cab5afbfee59f510131c18e98
paulosplima <155822549+paulosplima@users.noreply.github.com>
2026-01-07 03:20:40 +0000

    atualiza page1

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function HomePage() {
-  return <h1>NOVA PÁGINA EM CONSTRUÇÃO</h1>;
+  return (
+    <h3>
+      NOVA PÁGINA EM CONSTRUÇÃO !!! Em breve teremos muitas novidades para
+      você!Em breve teremos muitas novidades para você!
+    </h3>
+  );
   <h3>Em breve teremos muitas novidades para você!</h3>;
 }
+
 export default HomePage;
